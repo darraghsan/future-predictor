81df98b7f62a182b143b30b873d800295c2cfedf
Darragh Carroll <darraghcarroll01@gmail.com>
2020-12-17 16:10:58 +1100

    first commit

---
+++ future.js
@@ -0,0 +1,18 @@
+//allow user input
+const prompt = require('prompt-sync')({sigint: true});
+
+//get user age
+const age = prompt('Enter your age');
+
+
+const start = `Your future holds`;
+
+//predict death age
+
+//predict wealth
+
+//predict kids
+//array to hold positive words
+const positiveWords = [''];
+
+const negativeWords = [];
