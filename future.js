af47ac5227a646e2c29301e6a03edf62b8fdb296
Darragh Carroll <darraghcarroll01@gmail.com>
2020-12-17 16:49:06 +1100

    Added death age prediction

--- future.js
+++ future.js
@@ -1,15 +1,26 @@
 //allow user input
 const prompt = require('prompt-sync')({sigint: true});
 
+
+// ### Predict death age ###
+
 //get user age
 const age = prompt('Enter your age');
 
 
-const start = `Your future holds`;
+const min = Math.ceil(Number(age));
+const max = Math.floor(110);
 
-//predict death age
+//generate random death age
+const deathAge = Math.floor(Math.random() * (max - min) + min);
 
-//predict wealth
+console.log(`You will die at ${deathAge} years old.`);
+
+// ###########################
+
+
+
+// ### Predict wealth ###
 
 //predict kids
 //array to hold positive words
